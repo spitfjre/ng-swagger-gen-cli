15a2d8d8941c1c3a8a6bbb78523ec779fe36378b
spitfjre <spitfjre@gmx.de>
2019-04-21 11:14:26 +0200

    Compiled ts files.

--- ng-swagger-gen-cli-executor.js
+++ ng-swagger-gen-cli-executor.js
@@ -5,71 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var chalk_1 = __importDefault(require("chalk"));
 var execa_1 = __importDefault(require("execa"));
+var fs_1 = require("fs");
 var listr_1 = __importDefault(require("listr"));
 var md5_1 = __importDefault(require("md5"));
 var request_promise_native_1 = __importDefault(require("request-promise-native"));
-var fs_1 = require("fs");
-/**
- * Main generate function
- */
-function execute(options) {
-    if (!options.configurations || options.configurations.leading === 0) {
-        console.error('Configurations not specified');
-        process.exit(1);
-    }
-    if (!options.operation) {
-        console.error('No operation was specified');
-        process.exit(1);
-    }
-    if (options.selection !== undefined) {
-        var filteredConfigurations = options.configurations.filter(function (configuration) { return options.selection.indexOf(configuration.name) !== -1; });
-        if (filteredConfigurations.length === 0) {
-            console.error('No selected services were specified');
-            process.exit(1);
-        }
-        else {
-            executeOperation(options.operation, filteredConfigurations);
-        }
-    }
-    else {
-        executeOperation(options.operation, options.configurations);
-    }
-}
-exports.execute = execute;
-function executeOperation(operation, configurations) {
-    switch (operation) {
-        case 'compare':
-            compare(configurations);
-            break;
-        case 'generate':
-            generate(configurations);
-            break;
-        case 'update':
-            update(configurations);
-            break;
-        default:
-            console.error('No valid operation was specified');
-            process.exit(1);
-    }
-}
-function compare(apis) {
-    var checkedApis = {};
-    var tasks = new listr_1.default(apis.map(function (api) { return ({
-        title: "Compare " + api.name,
-        task: function () {
-            return request_promise_native_1.default(api.url).then(function (data) {
-                var currentSwaggerGenJson = fs_1.readFileSync(api.swaggerGen, 'utf8');
-                var swaggerPath = JSON.parse(currentSwaggerGenJson).swagger;
-                var currentJson = fs_1.readFileSync(swaggerPath, 'utf8');
-                var currentHash = md5_1.default(currentJson);
-                var fetchedHash = md5_1.default(data);
-                checkedApis[api.name] = fetchedHash === currentHash;
-            });
-        },
-    }); }), { concurrent: false, exitOnError: false });
-    tasks.run().then(function () { return presetCompareResult(apis, checkedApis); }, function () { return presetCompareResult(apis, checkedApis); });
-}
-function presetCompareResult(apis, checkedApis) {
+var presentCompareResult = function (apis, checkedApis) {
     var filteredApis = Object.entries(checkedApis)
         .map(function (entry) { return apis.find(function (api) { return api.name === entry[0] && entry[1] === false; }); })
         .filter(function (api) { return api !== undefined; })
@@ -85,15 +25,32 @@ function presetCompareResult(apis, checkedApis) {
             console.log("The changes for " + chalk_1.default.blue.bold(api.name) + " can be checked at " + chalk_1.default.green.bold(api.url) + ".");
         });
     }
-}
-function generate(apis) {
+};
+var compare = function (apis) {
+    var checkedApis = {};
+    var tasks = new listr_1.default(apis.map(function (api) { return ({
+        title: "Compare " + api.name,
+        task: function () {
+            return request_promise_native_1.default(api.url).then(function (data) {
+                var currentSwaggerGenJson = fs_1.readFileSync(api.swaggerGen, 'utf8');
+                var swaggerPath = JSON.parse(currentSwaggerGenJson).swagger;
+                var currentJson = fs_1.readFileSync(swaggerPath, 'utf8');
+                var currentHash = md5_1.default(currentJson);
+                var fetchedHash = md5_1.default(data);
+                checkedApis[api.name] = fetchedHash === currentHash;
+            });
+        },
+    }); }), { concurrent: false, exitOnError: false });
+    tasks.run().then(function () { return presentCompareResult(apis, checkedApis); }, function () { return presentCompareResult(apis, checkedApis); });
+};
+var generate = function (apis) {
     var tasks = new listr_1.default(apis.map(function (api) { return ({
         title: "Generate " + api.name,
         task: function () { return execa_1.default.stdout('./node_modules/.bin/ng-swagger-gen', ['-c', api.swaggerGen]).then(); },
     }); }), { concurrent: false, exitOnError: false });
     tasks.run().then(function () { }, function () { });
-}
-function update(apis) {
+};
+var update = function (apis) {
     var tasks = new listr_1.default(apis.map(function (api) { return ({
         title: "Update " + api.name,
         task: function () {
@@ -116,4 +73,43 @@ function update(apis) {
         },
     }); }), { concurrent: false, exitOnError: false });
     tasks.run().then(function () { }, function () { });
-}
+};
+var executeOperation = function (operation, configurations) {
+    switch (operation) {
+        case 'compare':
+            compare(configurations);
+            break;
+        case 'generate':
+            generate(configurations);
+            break;
+        case 'update':
+            update(configurations);
+            break;
+        default:
+            console.error('No valid operation was specified');
+            process.exit(1);
+    }
+};
+exports.execute = function (options) {
+    if (!options.configurations || options.configurations.length === 0) {
+        console.error('Configurations not specified');
+        process.exit(1);
+    }
+    if (!options.operation) {
+        console.error('No operation was specified');
+        process.exit(1);
+    }
+    if (options.selection !== undefined) {
+        var filteredConfigurations = options.configurations.filter(function (configuration) { return options.selection.indexOf(configuration.name) !== -1; });
+        if (filteredConfigurations.length === 0) {
+            console.error('No selected services were specified');
+            process.exit(1);
+        }
+        else {
+            executeOperation(options.operation, filteredConfigurations);
+        }
+    }
+    else {
+        executeOperation(options.operation, options.configurations);
+    }
+};
--- ng-swagger-gen-cli-parser.js
+++ ng-swagger-gen-cli-parser.js
@@ -1,10 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var argparse_1 = require("argparse");
-var ng_swagger_gen_cli_executor_1 = require("./ng-swagger-gen-cli-executor");
-var path_1 = require("path");
 var fs_1 = require("fs");
-function parse() {
+var path_1 = require("path");
+var ng_swagger_gen_cli_executor_1 = require("./ng-swagger-gen-cli-executor");
+var parseJSON = function (file) { return JSON.parse(fs_1.readFileSync(file, 'utf8')); };
+exports.parse = function () {
     var pkg = parseJSON(path_1.join(__dirname, 'package.json'));
     var argParser = new argparse_1.ArgumentParser({
         addHelp: true,
@@ -14,23 +15,26 @@ function parse() {
     argParser.addArgument(['-i', '--input'], {
         action: 'store',
         dest: 'config',
-        help: 'The swagger-gen CLI configuration file.',
+        help: 'The ng-swagger-gen-cli configuration file.',
+        required: true,
     });
     argParser.addArgument(['-o', '--operation'], {
         action: 'store',
         choices: ['compare', 'generate', 'update'],
         dest: 'operation',
         help: 'Desired operation that should be executed.',
+        required: true,
     });
     argParser.addArgument(['-s', '--selection'], {
         action: 'append',
         dest: 'selection',
         help: 'Selection of services, that should be operated on.',
+        required: false,
     });
     var args = argParser.parseArgs();
     if (fs_1.existsSync(args.config)) {
         var parsedConfig = parseJSON(args.config);
-        run({
+        ng_swagger_gen_cli_executor_1.execute({
             configurations: parsedConfig.configurations,
             operation: args.operation,
             selection: !!args.selection ? args.selection : undefined,
@@ -39,11 +43,4 @@ function parse() {
     else {
         argParser.parseArgs(['--help']);
     }
-}
-exports.parse = parse;
-function parseJSON(file) {
-    return JSON.parse(fs_1.readFileSync(file, 'utf8'));
-}
-function run(options) {
-    ng_swagger_gen_cli_executor_1.execute(options);
-}
+};
